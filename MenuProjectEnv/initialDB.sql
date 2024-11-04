0de2848b98d9f1e7c00d9f04aea5624aae239ca2
Kochu097 <maciek.kochman1@gmail.com>
2024-11-04 15:52:04 +0100

    database update

--- MenuProjectEnv/initialDB.sql
+++ MenuProjectEnv/initialDB.sql
@@ -1,55 +1,76 @@
-create table MEAL
-(
-    ID   int auto_increment
-        primary key,
-    Name varchar(50) not null
-);
+-- menu_database.USERS definition
 
-create table MENU
-(
-    ID  int auto_increment
-        primary key,
-    Day date not null
-);
+CREATE TABLE `USERS` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Authentication_Token` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table MENU_MEAL_TYPE
-(
-    ID        int auto_increment
-        primary key,
-    Meal_type varchar(50) not null
-);
 
-create table PRODUCT
-(
-    ID   int auto_increment
-        primary key,
-    Name varchar(50) not null
-);
+-- menu_database.MEAL definition
 
-create table MEAL_PRODUCT
-(
-    ID         int auto_increment
-        primary key,
-    Meal_ID    int null,
-    Product_ID int null,
-    constraint MEAL_PRODUCT_MEAL_ID_fk
-        foreign key (Meal_ID) references MEAL (ID),
-    constraint MEAL_PRODUCT_PRODUCT_ID_fk
-        foreign key (Product_ID) references PRODUCT (ID)
-);
+CREATE TABLE `MEAL` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `User_ID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `MEAL_Users_FK` (`User_ID`),
+  CONSTRAINT `MEAL_Users_FK` FOREIGN KEY (`User_ID`) REFERENCES `USERS` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create table MENU_ITEM
-(
-    ID         int auto_increment
-        primary key,
-    Menu_ID    int not null,
-    Meal_ID    int null,
-    Product_ID int null,
-    constraint MENU_ITEM_MEAL_ID_fk
-        foreign key (Meal_ID) references MEAL (ID),
-    constraint MENU_ITEM_MENU_ID_fk
-        foreign key (Menu_ID) references MENU (ID),
-    constraint MENU_ITEM_PRODUCT_ID_fk
-        foreign key (Product_ID) references PRODUCT (ID)
-);
 
+-- menu_database.MENU definition
+
+CREATE TABLE `MENU` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Day` date NOT NULL,
+  `User_ID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `MENU_Users_FK` (`User_ID`),
+  CONSTRAINT `MENU_Users_FK` FOREIGN KEY (`User_ID`) REFERENCES `USERS` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- menu_database.PRODUCT definition
+
+CREATE TABLE `PRODUCT` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `User_ID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `PRODUCT_Users_FK` (`User_ID`),
+  CONSTRAINT `PRODUCT_Users_FK` FOREIGN KEY (`User_ID`) REFERENCES `USERS` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- menu_database.MEAL_PRODUCT definition
+
+CREATE TABLE `MEAL_PRODUCT` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Meal_ID` int NOT NULL,
+  `Product_ID` int NOT NULL,
+  `Product_Amount` int NOT NULL,
+  `Product_Amount_Type` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `MEAL_PRODUCT_MEAL_ID_fk` (`Meal_ID`),
+  KEY `MEAL_PRODUCT_PRODUCT_ID_fk` (`Product_ID`),
+  CONSTRAINT `MEAL_PRODUCT_MEAL_ID_fk` FOREIGN KEY (`Meal_ID`) REFERENCES `MEAL` (`ID`),
+  CONSTRAINT `MEAL_PRODUCT_PRODUCT_ID_fk` FOREIGN KEY (`Product_ID`) REFERENCES `PRODUCT` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- menu_database.MENU_ITEM definition
+
+CREATE TABLE `MENU_ITEM` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Menu_ID` int NOT NULL,
+  `Meal_ID` int DEFAULT NULL,
+  `Product_ID` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `MENU_ITEM_MEAL_ID_fk` (`Meal_ID`),
+  KEY `MENU_ITEM_MENU_ID_fk` (`Menu_ID`),
+  KEY `MENU_ITEM_PRODUCT_ID_fk` (`Product_ID`),
+  CONSTRAINT `MENU_ITEM_MEAL_ID_fk` FOREIGN KEY (`Meal_ID`) REFERENCES `MEAL` (`ID`),
+  CONSTRAINT `MENU_ITEM_MENU_ID_fk` FOREIGN KEY (`Menu_ID`) REFERENCES `MENU` (`ID`),
+  CONSTRAINT `MENU_ITEM_PRODUCT_ID_fk` FOREIGN KEY (`Product_ID`) REFERENCES `PRODUCT` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
