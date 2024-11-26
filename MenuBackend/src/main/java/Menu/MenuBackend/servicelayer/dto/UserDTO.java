bfbe097d951da2a6516ea5b9f8b223cd2484daa5
Kochu097 <maciek.kochman1@gmail.com>
2024-11-26 16:50:13 +0100

    fix connection

--- MenuBackend/src/main/java/Menu/MenuBackend/servicelayer/dto/UserDTO.java
+++ MenuBackend/src/main/java/Menu/MenuBackend/servicelayer/dto/UserDTO.java
@@ -2,11 +2,8 @@ package Menu.MenuBackend.servicelayer.dto;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class UserDTO {
     private Integer id;
     private String firebaseUserId;
-    private List<MenuDTO> menus;
 }
